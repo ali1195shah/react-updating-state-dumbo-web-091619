12522c6d2d0a406ac3f55ac9ce9dc122c2273886
Ali Shah <ali1195shah@gmail.com>
2019-11-28 19:00:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 
 import ClickityClick from './components/ClickityClick';
 
-ReactDOM.render(<ClickityClick />, document.getElementById('root'));
+ReactDOM.render(<ClickityClick />, document.getElementById('root)
+);
